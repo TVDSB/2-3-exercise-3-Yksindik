8b728427226f054344af33b948ee0e0e3d70c0f3
Yksindik <100424625+Yksindik@users.noreply.github.com>
2022-04-12 14:03:16 -0400

    Update student.py

--- student.py
+++ student.py
@@ -1,6 +1,14 @@
 def main():
-    #your code goes here
-    print("0")
+    num = int(input("What is the Number"))
+
+    if num % 15 == 0:
+      print("FizzBuzz")
+    if num % 5 == 0:
+      print("buzz")
+    if num % 3 == 0:
+      print("Fizz")
+    else:
+      print(num)
 
 if __name__ =='__main__':
     main()
